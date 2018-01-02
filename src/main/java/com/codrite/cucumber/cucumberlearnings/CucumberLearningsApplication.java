bdf7c4ff1e6d3b5a43da0aff6cdd65c7badda00f
builttolast <builttolast@codrite.com>
2018-01-02 06:36:34 -0500

    Initial Commit

---
+++ src/main/java/com/codrite/cucumber/cucumberlearnings/CucumberLearningsApplication.java
@@ -0,0 +1,12 @@
+package com.codrite.cucumber.cucumberlearnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberLearningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberLearningsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codrite/cucumber/cucumberlearnings/CucumberLearningsApplicationTests.java
@@ -0,0 +1,25 @@
+package com.codrite.cucumber.cucumberlearnings;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("local")
+@Slf4j
+public class CucumberLearningsApplicationTests {
+
+	@Value("${test.env}")
+	private String testEnvironment;
+
+	@Test
+	public void contextLoads() {
+		log.debug(" Environment :  " + testEnvironment);
+	}
+
+}
